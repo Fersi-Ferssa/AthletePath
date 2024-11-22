d9bdda570e02b10b30021f0f63d92bda7ca3a874
Fernanda Ramirez Lopez <f.ramirez@lasallistas.org.mx>
2024-11-21 21:05:39 -0600

    9

--- athlete_path/production.py
+++ athlete_path/production.py
@@ -134,7 +134,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images)s
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
